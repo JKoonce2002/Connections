d903c73a7366079549e02bcf3800aa7f90e6c0d4
JKoonce2002 <71308122+JKoonce2002@users.noreply.github.com>
2024-09-02 21:50:51 -0500

    Export added to Connection class

--- src/connections/Connection.java
+++ src/connections/Connection.java
@@ -1,15 +1,22 @@
 package connections;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class Connection {
 	private String[][] groups;
 	private String[] groupNames;
+	private String name;
+	private int difficulty;
 	
 	//TODO: Figure out how to denote colors that need printing
 	
 	//Constructor
-	public Connection(String[][] groups, String[] groupNames) {
+	public Connection(String[][] groups, String[] groupNames, String name, int diff) {
 		this.groups = groups;
 		this.groupNames = groupNames;
+		this.name = name;
+		this.difficulty = diff;
 	}
 	
 	//Check a guess
@@ -44,8 +51,43 @@ public class Connection {
 		return this.groupNames[group];
 	}
 	
+	//return the Connection name
+	public String getName() {
+		return name;
+	}
+	
+	public int getDifficulty() {
+		return difficulty;
+	}
+	
 	//TODO: Export Connection as string to be saved in file
 	public void export() {
-		
+		try {
+			//Make the file writer
+			FileWriter fw = new FileWriter("C:\\Users\\" + System.getProperty("user.name")+"\\Desktop\\"+this.name+".cxn");
+			BufferedWriter writer = new BufferedWriter(fw);
+			
+			//Write the file
+			//TODO: Add encryption
+			writer.write(this.groupNames[0]);
+			writer.newLine();
+			writer.write(this.groups[0][0]+"/"+this.groups[0][1]+"/"+this.groups[0][2]+"/"+this.groups[0][3]);
+			writer.newLine();
+			writer.write(this.groupNames[1]);
+			writer.newLine();
+			writer.write(this.groups[1][0]+"/"+this.groups[1][1]+"/"+this.groups[1][2]+"/"+this.groups[1][3]);
+			writer.newLine();
+			writer.write(this.groupNames[2]);
+			writer.newLine();
+			writer.write(this.groups[2][0]+"/"+this.groups[2][1]+"/"+this.groups[2][2]+"/"+this.groups[2][3]);
+			writer.newLine();
+			writer.write(this.groupNames[3]);
+			writer.newLine();
+			writer.write(this.groups[3][0]+"/"+this.groups[3][1]+"/"+this.groups[3][2]+"/"+this.groups[3][3]);
+			
+			//Finish writing
+			writer.close();
+			
+		} catch (Exception ex) {ex.printStackTrace();}
 	}
 }
